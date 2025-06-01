a19a0e4c31313943d16d2397a07fd99fed7f5983
themagicmkt <132240308+themagicmkt@users.noreply.github.com>
2025-06-01 15:46:38 -0300

    fix: configura Vite para deploy na Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,18 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // ✅ ESSENCIAL PARA VER SERVE OS ASSETS
+  build: {
+    outDir: "dist" // ✅ NECESSÁRIO PARA VERCEL LOCALIZAR A PASTA DE OUTPUT
+  },
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
